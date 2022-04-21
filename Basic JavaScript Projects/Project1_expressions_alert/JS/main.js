068dbfa49579e309f04f47621c3379e9537451a1
kevinkeng7 <68366086+kevinkeng7@users.noreply.github.com>
2022-04-21 00:07:30 -0700

    Add files via upload

--- Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -1,8 +1,8 @@
-// alert("Hello, World -alert alert");
+alert("Hello, World -alert alert");  //alert box
 
-// window.alert("hello, world! -window alert"); 
+window.alert("hello, world! -window alert"); //window alert
 
-// document.write("hello, world! -document write method"); 
+document.write("hello, world!"); //document write method
 
 document.write("Lisa told Bart, \"Knock it off, Bart! Or I'll tell dad!\""); 
 
@@ -11,11 +11,12 @@ document.write("\"Be who you are and say what you feel," + "because those who ma
 var A = "This is a " + "Contatenated String " //This is contatenating a string
 document.write(A) 
 
-function My_First_Function() {
-    var str = "This text is green!";
-    var result = str.fontcolor("green");
-    document.getElementById("Green_Text").innerHTML = result;
+function My_First_Function() {   //Defining a function
+    var str = "This text is green!"; //Defining a variable and giving it a string value
+    var result = str.fontcolor("green"); //Using the fontcolor method on the string variable
+    document.getElementById("Green_Text").innerHTML = result; //Putting the value of result int HTML element with "Green_Text" id
 }
+
 
 var Family = "The Millers", Dad = "Dave" , Mom = "Mel" , Daughter = "Dora" , Son = "Sunny";
 var Family = Family.fontcolor("black");
@@ -26,6 +27,8 @@ var Son = Son.fontcolor("cyan");
 document.write(Son);
 
 document.write(3+3);
+
+
 
 // var blues = "I have the blues.";
 //var blues = blues.fontcolor("blue");
