64383918080e7b4bee9d39f63e25ad7da6fdf904
kevinkeng7 <68366086+kevinkeng7@users.noreply.github.com>
2022-04-20 23:46:37 -0700

    Add files via upload

---
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -0,0 +1,32 @@
+// alert("Hello, World -alert alert");
+
+// window.alert("hello, world! -window alert"); 
+
+// document.write("hello, world! -document write method"); 
+
+document.write("Lisa told Bart, \"Knock it off, Bart! Or I'll tell dad!\""); 
+
+document.write("\"Be who you are and say what you feel," + "because those who matter mind.\"" + "Dr. Suess"); 
+
+var A = "This is a " + "Contatenated String " //This is contatenating a string
+document.write(A) 
+
+function My_First_Function() {
+    var str = "This text is green!";
+    var result = str.fontcolor("green");
+    document.getElementById("Green_Text").innerHTML = result;
+}
+
+var Family = "The Millers", Dad = "Dave" , Mom = "Mel" , Daughter = "Dora" , Son = "Sunny";
+var Family = Family.fontcolor("black");
+var Dad = Dad.fontcolor("green");
+var Mom = Mom.fontcolor("red");
+var Daughter = Daughter.fontcolor("pink");
+var Son = Son.fontcolor("cyan");
+document.write(Son);
+
+document.write(3+3);
+
+// var blues = "I have the blues.";
+//var blues = blues.fontcolor("blue");
+//document.write(blues); 
