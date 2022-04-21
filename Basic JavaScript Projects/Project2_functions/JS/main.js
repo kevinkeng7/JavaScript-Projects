5dda7907edfe27cdb8318b873ae8f7176d61da3a
kevinkeng7 <68366086+kevinkeng7@users.noreply.github.com>
2022-04-21 00:29:05 -0700

    Add files via upload

---
+++ Basic JavaScript Projects/Project2_functions/JS/main.js
@@ -0,0 +1,11 @@
+function My_First_Function() {   //Defining a function
+    var str = "Welcome to Mcdonalds!"; //Defining a variable and giving it a string value
+    var result = str.fontcolor("purple"); //Using the fontcolor method on the string variable
+    document.getElementById("Purple_Text").innerHTML = result; //Putting the value of result int HTML element with "Green_Text" id
+}
+
+function myFunction() {
+    var sentence = "I am learning ";
+    sentence += "a lot from this book!";
+    document.getElementById("Concatenate").innerHTML = sentence;
+}
