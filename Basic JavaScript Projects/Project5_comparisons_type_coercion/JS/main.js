ab674f81c5ac0a12c14ff609d3f4c99f9f6eae21
kevinkeng7 <68366086+kevinkeng7@users.noreply.github.com>
2022-04-25 01:04:54 -0700

    Add files via upload

--- Basic JavaScript Projects/Project5_comparisons_type_coercion/JS/main.js
+++ Basic JavaScript Projects/Project5_comparisons_type_coercion/JS/main.js
@@ -29,3 +29,48 @@ function my_Function4()  {
 //document.write(2E310);
 
 //document.write(-3E310);
+
+document.write(10 > 2);
+document.write(10 < 2);
+
+console.log(12 + 12);
+
+document.write("10" + 5);
+
+console.log(10 < 2);
+
+document.write(10==10);
+
+document.write(3==11);
+
+
+A = 10;
+B = 10;
+document.write(A === B);
+
+C = 10;
+D = "five";
+document.write(C === D);
+
+E = 10;
+F = "ten";
+document.write(E === F);
+
+G = 10;
+H = 9;
+document.write (G === H);
+
+document.write(5 > 2 && 10 > 4);
+document.write(5 > 10 && 10 > 4);
+document.write(5 > 10 || 10 > 4);
+document.write(5 > 10 || 10 > 20);
+
+function not_Function0() {
+    document.getElementById("Not0").innerHTML = !(20 > 10);
+}
+
+function not_Function1() {
+    document.getElementById("Not1").innerHTML = !(5 > 10);
+}
+
+
