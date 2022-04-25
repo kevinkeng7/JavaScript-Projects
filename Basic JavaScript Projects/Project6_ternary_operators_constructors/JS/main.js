d551347bae875f46b2bd6ddfe09c2417be362a13
kevinkeng7 <68366086+kevinkeng7@users.noreply.github.com>
2022-04-25 16:09:20 -0700

    Add files via upload

--- Basic JavaScript Projects/Project6_ternary_operators_constructors/JS/main.js
+++ Basic JavaScript Projects/Project6_ternary_operators_constructors/JS/main.js
@@ -4,3 +4,13 @@ function Ride_Function() {
     Can_ride = (Height < 52) ? "You are too short":"You are tall enough";
     document.getElementById("Ride").innerHTML = Can_ride + " to ride.";
 }
+
+function nested_Function() {
+    document.getElementById("nested_Function").innerHTML = Count();
+    function Count() {
+        var Starting_point = 9;         //start counting from 9
+        function Plus_one() {Starting_point += 1;};     //add 1 to the starting value
+        Plus_one ();
+        return Starting_point;
+    }
+}
