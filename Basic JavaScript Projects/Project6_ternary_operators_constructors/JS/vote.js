d1581c6da6c4d7f528d561312041e5072581126a
kevinkeng7 <68366086+kevinkeng7@users.noreply.github.com>
2022-04-25 02:12:09 -0700

    Add files via upload

---
+++ Basic JavaScript Projects/Project6_ternary_operators_constructors/JS/vote.js
@@ -0,0 +1,6 @@
+function Age_Function() {
+    var Age, Can_vote;
+    Age = document.getElementById("Age").value;
+    Can_vote = (Age < 18) ? "You are not old enough":"You are old enough";
+    document.getElementById("Vote").innerHTML = Can_vote + " to vote.";
+}
